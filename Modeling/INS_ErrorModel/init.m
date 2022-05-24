dfa64f0a2860d53288f9471fc41fb49c61bf864b
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2022-05-25 00:20:35 +0300

    Added solution of the reverse navigation problem for parking debug model

--- Modeling/INS_ErrorModel/init.m
+++ Modeling/INS_ErrorModel/init.m
@@ -1,15 +1,15 @@
 %init inertial sensors errors
-gyro_drift_const = deg2rad(0.02)/3600;
-accel_zero_offset = 5e-3;
+gyro_drift_const = deg2rad(0.015)/3600;
+accel_zero_offset = 5e-4;
 gyro_scale_factor = 5e-5;
 accel_scale_factor = 3e-6;
 gyro_noise_deviation = deg2rad(0.01)/3600;
-accel_noise_deviation = 1e-3;
+accel_noise_deviation = 1e-4;
 
 %init corrector errors
-lat_error_deviation = 1.5;
-lon_error_deviation = 1.5;
-alt_error_deviation = 1.5;
-vel_e_error_deviation = 0.01;
-vel_n_error_deviation = 0.01;
-vel_h_error_deviation = 0.01;
+lat_error_deviation = 15;
+lon_error_deviation = 15;
+alt_error_deviation = 15;
+vel_e_error_deviation = 0.1;
+vel_n_error_deviation = 0.1;
+vel_h_error_deviation = 0.1;
